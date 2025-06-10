0fb4dfb1b06e757373fde0024a95811b9177654a
rtwair@gmail.com <riyad@rtp.cc>
2025-06-10 19:08:34 +0800

    more harpoon nav stuff

--- lua/rtwair/after.lua
+++ lua/rtwair/after.lua
@@ -4,6 +4,8 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
 
 -- undo tree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
