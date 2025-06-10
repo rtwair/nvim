914001b890340e97a68997d5bb7b5a4febad6413
rtwair@gmail.com <riyad@rtp.cc>
2025-06-11 04:17:10 +0800

    added new neovim auto-toggle when file opens (starting to like netrw lol)

--- lua/rtwair/after.lua
+++ lua/rtwair/after.lua
@@ -4,13 +4,34 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>1", function()
+	ui.nav_file(1)
+end)
+vim.keymap.set("n", "<leader>2", function()
+	ui.nav_file(2)
+end)
+vim.keymap.set("n", "<leader>3", function()
+	ui.nav_file(3)
+end)
+vim.keymap.set("n", "<leader>4", function()
+	ui.nav_file(4)
+end)
 
 -- undo tree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- vim fugitave
 vim.keymap.set("n", "<leader>g", "<cmd>Git<CR>")
+
+-- neotree auto close after file
+require("neo-tree").setup({
+	event_handlers = {
+		{
+			event = "file_opened",
+			handler = function(file_path)
+				-- close neo-tree
+				require("neo-tree.command").execute({ action = "close" })
+			end,
+		},
+	},
+})
