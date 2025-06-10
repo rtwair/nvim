90bcca83ebc29c8d002bc0993f9df1d3e61d9154
rtwair@gmail.com <riyad@rtp.cc>
2025-06-11 02:34:31 +0800

    Added lspconfig definitions for lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,28 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.gopls.setup({})
-        lspconfig.clangd.setup({})
-    end,
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
+		lspconfig.gopls.setup({})
+		lspconfig.clangd.setup({})
+	end,
+	-- Configure the Lua LSP
+	require("lspconfig").lua_ls.setup({
+		settings = {
+			Lua = {
+				runtime = {
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	}),
 }
